23f97855066a499ffb623029eb07f9edeb3b557f
Patryk Stronski <p.stronski13@gmail.com>
2021-10-16 18:50:59 +0200

    done

--- index2.py
+++ index2.py
@@ -32,28 +32,3 @@ plt.legend()
 plt.grid(True)
 plt.title('Data and its linear approximation')
 plt.show()
-
-print('Rational approximant')
-plt.scatter(X_ES, NOISY_DATA, label='Y noisy data')
-
-answ = exhaustive_search(rational_approximation, START, END, PRECISION)
-print(f"Exhaustive Search: result={answ[0]} a={answ[1][0]}; b={answ[1][1]};function_calculation={answ[2]}; iterations={answ[3]}")
-
-plt.plot(X_ES, [linear_approximation(x, answ[1][0], answ[1][1]) for x in X_ES], label='exhaustive search')
-
-answ = gauss_search(rational_approximation, START, END, PRECISION)
-print(f"Gauss Search: result={answ[0]} a={answ[1][0]}; b={answ[1][1]};function_calculation={answ[2]}; iterations={answ[3]}")
-
-plt.plot(X_ES, [linear_approximation(x, answ[1][0], answ[1][1]) for x in X_ES], label='Gauss coordinate descent')
-
-answ = nelder_mead(rational_approximation, START, END, PRECISION)
-print(f"Nelder-Mead Search: result={answ[0]} a={answ[1][0]}; b={answ[1][1]};function_calculation={answ[2]}; iterations={answ[3]}")
-
-plt.plot(X_ES, [linear_approximation(x, answ[1][0], answ[1][1]) for x in X_ES], label='Nelder-Mead method')
-
-plt.xlabel('K-value')
-plt.ylabel('Y-value')
-plt.legend()
-plt.grid(True)
-plt.title('Data and its linear approximation')
-plt.show()
